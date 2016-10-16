8c18fd78f2c39da5e8f0eb183abb3252e26b148f
lijing <lijing94@outlook.com>
2016-10-16 22:29:32 +0800

    10.16添加搜索页面通不过编译版本

---
+++ app/src/main/java/com/example/administrator/zhihucopy/first_search.java
@@ -0,0 +1,12 @@
+package com.example.administrator.zhihucopy;
+
+import android.app.Activity;
+import android.support.v7.widget.SearchView;
+
+/**
+ * Created by Administrator on 2016/10/16 0016.
+ * 首页搜索界面代码，布局中含有一个搜索框、按钮、listview
+ */
+
+public class first_search extends Activity {
+}
